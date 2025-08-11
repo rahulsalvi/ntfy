0e3d6a6f009e5c9fa81689a02d3005c21346a8a8
Rahul Salvi <rsalvi006@gmail.com>
2025-08-10 21:15:18 -0700

    update to new docker-compose-snippets and .env for secrets

---
+++ pre-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+TS_TAGS=tag:webserver
+
+# return code
+RC=0
+
+git submodule update --init --recursive
+
+if [ ! -e .env ]; then
+    touch .env
+fi
+chmod -f 600 .env
+
+if ! grep -q "TS_AUTHKEY" .env; then
+    if [ -z "$TS_API_CLIENT_ID" ]; then
+        echo "ERROR: no tailscale oauth credentials provided"
+        RC=1
+    else
+        echo "Adding TS_AUTHKEY to .env"
+        echo "TS_AUTHKEY=$(get-authkey -tags "$TS_TAGS" -ephemeral -preauth)" >>.env
+    fi
+fi
+
+exit $RC
